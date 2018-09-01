38f9dc8ffc0262d6e10d8b20cc9e8a2327a5f6ef
Jhonatan Teixeira <johnn_lds@hotmail.com>
2018-09-01 01:13:49 -0300

    configure routes for goals, vendors

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
     get '/' => 'home#index'
-    resources :stores
+    resources :stores do
+      resources :goals
+    end
+    resources :vendors
   end
 end
