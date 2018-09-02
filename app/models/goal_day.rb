6d4d32d90f6f22137a60d5f9486edc1ac098346e
Jhonatan Teixeira <johnn_lds@hotmail.com>
2018-09-02 16:52:30 -0300

    remove unecessary files

--- app/models/goal_day.rb
+++
@@ -1,4 +0,0 @@
-class GoalDay < ApplicationRecord
-  belongs_to :goal
-  has_many :vendors
-end
--- app/models/goal_day_vendor.rb
+++
@@ -1,4 +0,0 @@
-class GoalDayVendor < ApplicationRecord
-  belongs_to :goal_day
-  has_many :vendors
-end
